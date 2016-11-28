8d7c99e1327228fdcd911da42d07a472bf2c22ec
Halimat Mercy Oseni <mehrceey04@gmail.com>
2016-11-29 00:51:22 +0100

    added codes for word count lab

---
+++ app/wordCount.js
@@ -0,0 +1,17 @@
+module.exports = {
+  wordCount : function(word) {
+  objCount = {};
+  splitWords = word.split(/[\s]+/);
+  splitWords.forEach(function(words) {
+    if(objCount.hasOwnProperty(words)) {
+      objCount[words]++;
+      }
+    else {
+      objCount[words] = 1;
+      }
+      });
+    
+  return objCount;
+}
+}
+
